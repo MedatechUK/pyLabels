862f6e887386520d885378631acf36c0d0e43d08
Simon Barnett <simonbarnett@emerge-it.co.uk>
2024-06-03 13:19:59 +0100

    chore: Update build script to use mklabel.spec for pyinstaller

--- build.bat
+++ build.bat
@@ -1,6 +1,1 @@
-cd M:\python\ap
-pyuic6 -x Landlord.ui -o output.py
-pyinstaller --noconfirm --onefile Landlord.spec
-MD M:\python\ap\dist\Landlord\pylabels
-copy M:\python\ap\pylabels\*.* M:\python\ap\dist\Landlord\pylabels
-dist\Landlord\Landlord.exe
+pyinstaller --noconfirm mklabel.spec
